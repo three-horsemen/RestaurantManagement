3f3c7c4b5f2f3769b1920f969bd7ecc2c07f6a17
Soorya Annadurai <soorya.annadurai@gmail.com>
2017-11-02 19:53:33 +0530

    Added last-access description with invisibility functionality

--- RestaurantManagement/MasterPage.master.cs
+++ RestaurantManagement/MasterPage.master.cs
@@ -9,5 +9,34 @@ public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            clearCookieButton.Visible = false;
+            bool setFlag = false;
+            if (Request.Cookies["Luigis"] != null)
+            {
+                if (Request.Cookies["Luigis"]["lastAccess"] != null)
+                {
+                    lastAccessLabel.Text = "Last accessed at: " + Request.Cookies["Luigis"]["lastAccess"].ToString();
+                    clearCookieButton.Visible = true;
+                    setFlag = true;
+                }
+            }
+            if(!setFlag)
+            {
+                lastAccessLabel.Text = "New user detected!";
+            }
+            Response.Cookies["Luigis"]["lastAccess"] = DateTime.Now.ToString();
+            Response.Cookies["Luigis"].Expires = DateTime.Now.AddDays(1d);
+        }
+        
+    }
+
+    protected void clearCookieButton_Click(object sender, EventArgs e)
+    {
+        Response.Cookies["Luigis"].Expires = DateTime.Now.AddDays(-1d);
+        lastAccessLabel.Text = "Erased user presence!";
+        clearCookieButton.Visible = false;
+        //Server.TransferRequest(Request.Url.AbsolutePath, false);
     }
 }
