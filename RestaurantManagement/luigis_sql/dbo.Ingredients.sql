be9d7c49481585fe77991b736269d921d85d2f7a
Soorya Annadurai <soorya.annadurai@gmail.com>
2017-11-03 00:43:13 +0530

    Added ingredient_quantity attribute to Ingredients table. Revamped sample data for Ingredients.

--- RestaurantManagement/luigis_sql/dbo.Ingredients.sql
+++ RestaurantManagement/luigis_sql/dbo.Ingredients.sql
@@ -2,5 +2,6 @@
 (
 	[ingredient_id] INT NOT NULL PRIMARY KEY, 
     [ingredient_name] VARCHAR(50) NOT NULL, 
-    [ingredient_price] DECIMAL NOT NULL
+    [ingredient_price] DECIMAL NOT NULL,
+	[ingredient_quantity] INT NOT NULL
 )
--- RestaurantManagement/luigis_sql/sample_data/dbo.IngredientsData.sql
+++ RestaurantManagement/luigis_sql/sample_data/dbo.IngredientsData.sql
@@ -1,16 +1,16 @@
 ﻿CREATE PROCEDURE [dbo].[IngredientsData]
 AS
 	DELETE FROM Ingredients;
-	INSERT INTO Ingredients(ingredient_id, ingredient_name, ingredient_price)
+	INSERT INTO Ingredients(ingredient_id, ingredient_name, ingredient_price, ingredient_quantity)
 	VALUES
-	(1, 'Salt', 10),
-	(2,	'Bread', 50),
-	(3,	'Sugar', 15),
-	(4,	'Milk', 20),
-	(5,	'Chili', 5),
-	(6,	'Onion', 10),
-	(7,	'Cheese', 15),
-	(8,	'Capsicum', 30),
-	(9,	'Potato', 25),
-	(10, 'Pepper', 10)
+	(1, 'Salt', 10, 8),
+	(2,	'Bread', 50, 8),
+	(3,	'Sugar', 15, 8),
+	(4,	'Milk', 20, 8),
+	(5,	'Chili', 5, 8),
+	(6,	'Onion', 10, 8),
+	(7,	'Cheese', 15, 8),
+	(8,	'Capsicum', 30, 8),
+	(9,	'Potato', 25, 8),
+	(10, 'Pepper', 10, 8)
 RETURN 0
