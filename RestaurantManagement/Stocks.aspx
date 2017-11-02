ba504c1e0fcd47c7d80150aa25e1f2b9876802d4
Soorya Annadurai <soorya.annadurai@gmail.com>
2017-11-03 02:13:22 +0530

    Completed stock management, ingredient replenishing, and cash updation on ingredient replenishing

--- RestaurantManagement/Stocks.aspx
+++ RestaurantManagement/Stocks.aspx
@@ -6,10 +6,38 @@
     <h2>Stocks</h2>
     <h3>Average Daily Consumption</h3>
     <asp:GridView ID="consumption" DataSourceID="consumptionSource" runat="server"></asp:GridView>
-    <h3>Additional incredients required</h3>
-    <asp:GridView ID="ingredientsRequired" DataSourceID="incredientsRequiredSource" runat="server"></asp:GridView>
+    <h3>Additional ingredients required</h3>
+    <asp:GridView ID="ingredientsRequired" DataSourceID="ingredientsRequiredSource" runat="server" OnDataBound="ingredientsRequired_DataBound"></asp:GridView>
+    Total replenish cost = <asp:Label ID="totalReplenishCost" runat="server"></asp:Label>
+    <br />
+    Current cash = <asp:Label ID="currentCash" runat="server"></asp:Label>
+    <br />
     <asp:Button ID="purchaseIngredients" runat="server" Text="Purchase" OnClick="purchaseIngredients_Click" />
-    <asp:SqlDataSource ID="consumptionSource" runat="server" ConnectionString="<%$connectionStrings:luigis %>"></asp:SqlDataSource>
-    <asp:SqlDataSource ID="incredientsRequiredSource" runat="server" ConnectionString="<%$connectionStrings:luigis %>"></asp:SqlDataSource>
-    
+    <asp:Label ID="confirmPurchase" runat="server"></asp:Label>
+
+    <asp:SqlDataSource ID="consumptionSource" runat="server" ConnectionString="<%$connectionStrings:luigis %>"
+        SelectCommand="
+select
+	Ingredients.ingredient_name, ceiling(sum(orders.quantity * recipe.ingredient_quantity)/3.0) as avg_ingredient_qty_3_days
+from 
+	orders, items, recipe, Ingredients
+where 
+	orders.item_id = items.item_id and items.item_id = recipe.item_id and recipe.ingredient_id = Ingredients.ingredient_id and Orders.timestamp Between DATEADD(d, -3, GETDATE()) and GETDATE()
+group by
+	Ingredients.ingredient_name">
+
+    </asp:SqlDataSource>
+    <asp:SqlDataSource ID="ingredientsRequiredSource" runat="server" ConnectionString="<%$connectionStrings:luigis %>"
+        SelectCommand="
+select 
+	Ingredients.ingredient_name, ceiling(sum(orders.quantity * recipe.ingredient_quantity)/1.5 - Ingredients.ingredient_quantity) as future_requirement, ceiling(sum(orders.quantity * recipe.ingredient_quantity)/1.5 - Ingredients.ingredient_quantity) * Ingredients.ingredient_price as replenish_cost
+from 
+	orders, items, recipe, Ingredients
+where 
+	orders.item_id = items.item_id and items.item_id = recipe.item_id and recipe.ingredient_id = Ingredients.ingredient_id and Orders.timestamp Between DATEADD(d, -3, GETDATE()) and GETDATE()
+group by
+	Ingredients.ingredient_name, Ingredients.ingredient_quantity, Ingredients.ingredient_price
+having
+	ceiling(sum(orders.quantity * recipe.ingredient_quantity)/1.5 - Ingredients.ingredient_quantity) > 0
+"></asp:SqlDataSource>
 </asp:Content>
--- RestaurantManagement/Stocks.aspx.cs
+++ RestaurantManagement/Stocks.aspx.cs
@@ -4,16 +4,102 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Web.Configuration;
 
 public partial class _Default : System.Web.UI.Page
 {
+    public int totalValue = 0;
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            totalValue = 0;
+            totalReplenishCost.Text = "";
 
+            string selectSql = "select cash from Store";
+            string connStr = WebConfigurationManager.ConnectionStrings["luigis"].ConnectionString;
+            SqlConnection con = new SqlConnection(connStr);
+            SqlCommand cmd = new SqlCommand(selectSql, con);
+            SqlDataReader r;
+            try
+            {
+                con.Open();
+                r = cmd.ExecuteReader();
+                while (r.Read())
+                {
+                    currentCash.Text = r["cash"].ToString();
+                }
+                r.Close();
+            }
+            catch
+            {
+                currentCash.Text = "NA";
+            }
+            finally
+            {
+                con.Close();
+            }
+            purchaseIngredients.Enabled = false;
+        }
     }
 
     protected void purchaseIngredients_Click(object sender, EventArgs e)
     {
+        string updateSql = "update Store set cash = cash - " + totalReplenishCost.Text;
+        string connStr = WebConfigurationManager.ConnectionStrings["luigis"].ConnectionString;
+        SqlConnection con = new SqlConnection(connStr);
+        SqlCommand cmd = new SqlCommand(updateSql, con);
+        try
+        {
+            con.Open();
+            int affected = cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            confirmPurchase.Text = "Purchase complete!";
+            try
+            {
+                int int_currentCash = 0;
+                int_currentCash = (Convert.ToInt32(currentCash.Text) - Convert.ToInt32(totalReplenishCost.Text));
+                currentCash.Text = (Convert.ToInt32(currentCash.Text) - Convert.ToInt32(totalReplenishCost.Text)).ToString();
+                if (int_currentCash < Convert.ToInt32(totalReplenishCost.Text))
+                {
+                    purchaseIngredients.Enabled = false;
+                }
+            }
+            catch
+            {
+                currentCash.Text = "Encountered an error!";
+            }
+        }
+        catch
+        {
+            confirmPurchase.Text = "Purchase failed!";
+        }
+        finally
+        {
+            confirmPurchase.Text += " Time: " + DateTime.Now.ToString();
+            con.Close();
+        }
+    }
 
+    protected void ingredientsRequired_DataBound(object sender, EventArgs e)
+    {
+        foreach (GridViewRow Row in ingredientsRequired.Rows)
+        {
+            if (Row.RowType == DataControlRowType.DataRow)
+            {
+                totalValue += Convert.ToInt32(Row.Cells[2].Text);
+            }
+        }
+        totalReplenishCost.Text = totalValue.ToString();
+        try
+        {
+            if (totalValue <= Convert.ToInt32(currentCash.Text))
+            {
+                purchaseIngredients.Enabled = true;
+            }
+        }
+        catch {}
     }
 }
